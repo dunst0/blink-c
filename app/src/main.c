b92805a7cbccf988c974a179bbee96fed3f47d5f
Rick Barenthin <rick@ng-voice.com>
2020-06-24 23:56:13 +0200

    Save state of flex and bison

--- app/src/main.c
+++ app/src/main.c
@@ -1,26 +1,45 @@
 #include "blink/parser.h"
+#include "blink/str.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char **argv) {
+    extern FILE *yyin;
 
-    int length = snprintf(NULL, 0, "%s\n", "stdin");
-    filename = calloc(length + 1, sizeof(char));
-    if (!filename) {
-        yyerror("out of memory");
-        exit(1);
+    char *sourceFile = "stdin";
+    str sourceFileName = STR_NULL;
+    int loadFromFile = 0;
+
+    if (argc == 2) {
+        sourceFile = argv[1];
+        loadFromFile = 1;
     }
-    snprintf(filename, length + 1, "stdin");
+
+    sourceFileName.len = snprintf(NULL, 0, "%s", sourceFile) + 1;
+    sourceFileName.s = calloc(sourceFileName.len, sizeof(*sourceFileName.s));
+    if (!sourceFileName.s) {
+        fprintf(stderr, "Error: could not allocate memory for filename\n");
+        return 2;
+    }
+    snprintf(sourceFileName.s, sourceFileName.len, "%s", sourceFile);
+
+    if (loadFromFile) {
+        yyin = fopen(sourceFileName.s, "r");
+    } else {
+        yyin = stdin;
+    }
+
+    printf("%s: start parsing from '%.*s'\n", argv[0], STR_FMT(&sourceFileName));
 
     yydebug = 1;
+    filename = sourceFileName.s;
 
-    if (!yyparse()) {
-        printf("blink parse worked\n");
-    } else {
+    if (yyparse() != 0) {
         printf("blink parse failed\n");
+        return 1;
     }
+    printf("blink parse worked\n");
 
     return 0;
 }
