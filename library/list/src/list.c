424a938ccf1ae21ca2de215b93f4311b902e1b8d
dunst0 <dunst0@gmail.com>
2021-07-05 10:52:32 +0200

    wip(list): hide the actual type and export information over functions

--- library/list/src/list.c
+++ library/list/src/list.c
@@ -11,11 +11,64 @@
 
 
 // -----------------------------------------------------------------------------
+//  Local types
+// -----------------------------------------------------------------------------
+
+/**
+ * @brief Struct representing a List node.
+ */
+struct waitui_list_node {
+    void *element;
+    waitui_list_node *next;
+    waitui_list_node *prev;
+};
+
+/**
+ * @brief Struct representing a List.
+ */
+struct waitui_list {
+    waitui_list_node *head;
+    waitui_list_node *tail;
+    waitui_list_element_destroy elementDestroyCallback;
+    unsigned long long length;
+};
+
+/**
+ * @brief Struct representing a List iterator.
+ */
+struct waitui_list_iter {
+    waitui_list_node *node;
+};
+
+
+// -----------------------------------------------------------------------------
+//  Local functions
+// -----------------------------------------------------------------------------
+
+/**
+ * @brief Create a List iterator.
+ * @param[in] node The node to use as a start for the iterator
+ * @return A pointer to waitui_list_iter or NULL if memory allocation failed
+ */
+static waitui_list_iter *waitui_list_iter_new(waitui_list_node *node) {
+    waitui_list_iter *this = NULL;
+
+    this = calloc(1, sizeof(*this));
+    if (!this) { return NULL; }
+
+    this->node = node;
+
+    return this;
+}
+
+
+// -----------------------------------------------------------------------------
 //  Public functions
 // -----------------------------------------------------------------------------
 
-list *list_new(list_element_destroy elementDestroyCallback) {
-    list *this = NULL;
+waitui_list *
+waitui_list_new(waitui_list_element_destroy elementDestroyCallback) {
+    waitui_list *this = NULL;
 
     this = calloc(1, sizeof(*this));
     if (!this) { return NULL; }
@@ -25,12 +78,12 @@ list *list_new(list_element_destroy elementDestroyCallback) {
     return this;
 }
 
-void list_destroy(list **this) {
+void waitui_list_destroy(waitui_list **this) {
     if (!this || !(*this)) { return; }
 
     while ((*this)->head) {
-        list_node *temp = (*this)->head;
-        (*this)->head   = (*this)->head->next;
+        waitui_list_node *temp = (*this)->head;
+        (*this)->head          = (*this)->head->next;
 
         if ((*this)->elementDestroyCallback) {
             (*this)->elementDestroyCallback(&temp->element);
@@ -42,8 +95,8 @@ void list_destroy(list **this) {
     *this = NULL;
 }
 
-int list_push(list *this, void *element) {
-    list_node *node = NULL;
+int waitui_list_push(waitui_list *this, void *element) {
+    waitui_list_node *node = NULL;
 
     if (!this || !element) { return 0; }
 
@@ -65,9 +118,9 @@ int list_push(list *this, void *element) {
     return 1;
 }
 
-void *list_pop(list *this) {
-    list_node *node = NULL;
-    void *element   = NULL;
+void *waitui_list_pop(waitui_list *this) {
+    waitui_list_node *node = NULL;
+    void *element          = NULL;
 
     if (!this || this->length == 0) { return NULL; }
 
@@ -87,8 +140,8 @@ void *list_pop(list *this) {
     return element;
 }
 
-int list_unshift(list *this, void *element) {
-    list_node *node = NULL;
+int waitui_list_unshift(waitui_list *this, void *element) {
+    waitui_list_node *node = NULL;
 
     if (!this || !element) { return 0; }
 
@@ -110,9 +163,9 @@ int list_unshift(list *this, void *element) {
     return 1;
 }
 
-void *list_shift(list *this) {
-    list_node *node = NULL;
-    void *element   = NULL;
+void *waitui_list_shift(waitui_list *this) {
+    waitui_list_node *node = NULL;
+    void *element          = NULL;
 
     if (!this || this->length == 0) { return NULL; }
 
@@ -130,4 +183,37 @@ void *list_shift(list *this) {
     free(node);
 
     return element;
+}
+
+void *waitui_list_peek(waitui_list *this) {
+    if (!this || this->length == 0) { return NULL; }
+    return this->head->element;
+}
+
+waitui_list_iter *waitui_list_getIterator(waitui_list *this) {
+    if (!this) { return NULL; }
+    return waitui_list_iter_new(this->head);
+}
+
+bool waitui_list_iter_hasNext(waitui_list_iter *this) {
+    if (!this) { return false; }
+    return this->node != NULL;
+}
+
+void *waitui_list_iter_next(waitui_list_iter *this) {
+    waitui_list_node *node = NULL;
+
+    if (!this) { return NULL; }
+
+    node       = this->node;
+    this->node = this->node->next;
+
+    return node;
+}
+
+void waitui_list_iter_destroy(waitui_list_iter **this) {
+    if (!this || !(*this)) { return; }
+
+    free(*this);
+    *this = NULL;
 }
